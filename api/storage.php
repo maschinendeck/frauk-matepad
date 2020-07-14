639c5c7b70dfe53e596ee1c53e183f9d874610d7
Eileen Neumann <eileen@maschinendeck.org>
2020-07-14 20:51:30 +0200

    Added basic site layout

---
+++ api/storage.php
@@ -0,0 +1,70 @@
+<?php
+	class UserData {
+		public $id;
+		public $name;
+		public $balance;
+	}
+
+	class ItemData {
+		public $id;
+		public $name;
+		public $costs;
+	}
+
+	class Storage {
+		private $userList = array();
+		private $itemList = array();
+
+		// ID generator
+		private function makeNewRandomID() {
+			return sha1(random_bytes(32));
+		}
+
+		// Item Functions
+		public function createItem($item) {
+			$item->id = $this->makeNewRandomID();
+			array_push($this->itemList, $item);
+		}
+
+		public function getAllItems() {
+			return $this->itemList;
+		}
+
+		public function removeItem($item) {
+			foreach ($this->itemList as $key => $value) {
+				if ($value->id == $item->id) {
+					unset($key);
+				}
+			}
+		}
+
+		// User Functions
+		public function createUser($user) {
+			$user->id = $this->makeNewRandomID();
+			array_push($this->userList, $user);
+		}
+
+		public function getAllUser() {
+			return $this->userList;
+		}
+
+		public function removeUser($user) {
+			foreach ($this->userList as $key => $value) {
+				if ($value->id == $item->id) {
+					unset($key);
+				}
+			}
+		}
+
+		// Serialization
+		public function writeToDisk() {
+			file_put_contents("user.json", json_encode($this->userList));
+			file_put_contents("item.json", json_encode($this->itemList));
+		}
+
+		public function readFromDisk() {
+			$this->userList = json_decode(file_get_contents("user.json"));
+			$this->itemList = json_decode(file_get_contents("item.json"));
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,55 @@
 <?php
-echo "Test"
+	include_once "api/storage.php";
+
+	define("PATH_INCLUDE_PAGES",    "./pages/");
+	define("PATH_INCLUDE_THEME",    "./theme/");
+	define("PATH_INCLUDE_INDEX",    "index");
+	define("PATH_INCLUDE_404",      "file_not_found");
+	define("REQUEST_GET_PAGE_NAME", "page");
+	define("THEME_FILE_HEAD",       "head");
+	define("THEME_FILE_TAIL",       "tail");
+
+	function loadPage($page, $plainError = false, $path = PATH_INCLUDE_PAGES) {
+		$requestPage = $path . $page . ".html";
+		if (file_exists($requestPage)) {
+			include_once($requestPage);
+		} else {
+			if ($plainError) {
+				echo "<center>Error Loading Page - 404 File Not Found</center>";
+			} else {
+				load404();
+			}
+		}
+	}
+
+	function load404() {
+		loadPage(PATH_INCLUDE_404, $plainError = true);
+	}
+
+	function loadIndex() {
+		loadPage(PATH_INCLUDE_INDEX);
+	}
+
+	function sanitizePageParameter($page) {
+		$info = pathinfo($page);
+		if (isset($info['extension'])) {
+			return basename($page, "." .$info['extension']);
+		} else {
+			return basename($page);
+		}
+	}
+
+	# Import head
+	loadPage(THEME_FILE_HEAD, '', $path = PATH_INCLUDE_THEME);
+
+	# Load requested file
+	if (isset($_GET[REQUEST_GET_PAGE_NAME])) {
+		$pageToLoad = sanitizePageParameter($_GET[REQUEST_GET_PAGE_NAME]);
+		loadPage($pageToLoad);
+	} else {
+		loadIndex();
+	}
+
+	# Import tail
+	loadPage(THEME_FILE_TAIL, '', $path = PATH_INCLUDE_THEME);
 ?>
