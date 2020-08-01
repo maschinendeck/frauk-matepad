9d4bc3f36b2c17e9dbf01960b81f655032692cc5
Eileen Neumann <eileen@maschinendeck.org>
2020-08-02 00:42:51 +0200

    Fixed broken link in user signout

--- pages/index/page.php
+++ pages/index/page.php
@@ -1,12 +1,11 @@
 <?php
 
-    $bindings = array();
-    $bindings["name"] = $currentUser ? $currentUser->name : "No User";
-
     foreach ($store->getAllItems() as $item) {
         $bindings = array("itemid" => $item->id, "name" => $item->name, "costs" => $item->costs);
         bindAndRenderTemplate(__DIR__ . "/template_item.html", $bindings);
     }
 
+    $bindings = array();
+    $bindings["name"] = $currentUser ? $currentUser->name : "No User";
     bindAndRenderTemplate(__DIR__ . "/template.html", $bindings);
 ?>
