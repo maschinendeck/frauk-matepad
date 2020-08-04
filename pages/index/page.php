b58baea0544a3d6ed8c70fde4a30ef78ce44c1b4
Eileen Neumann <eileen@maschinendeck.org>
2020-08-05 00:04:28 +0200

    Added buy confirmation

--- pages/index/page.php
+++ pages/index/page.php
@@ -7,7 +7,8 @@
     bindAndRenderTemplate(__DIR__ . "/template_item.html", $bindingsCharge);
 
     foreach ($store->getAllItems() as $item) {
-        $bindings = array(  "href" => $currentUser ? "?page=item_buy&id=" . $item->id : "?page=user_signin",
+        $bindings = array(  "id" => $item->id,
+                            "href" => $currentUser ? "?page=item_buy&id=" . $item->id : "?page=user_signin",
                             "name" => $item->name, 
                             "price" => is_numeric($item->costs) ? number_format($item->costs / 100.0, 2) . "€" : $item->costs,
                             "image" => $item->image);
