98c9344a01cb42b94f0c0b470bf666101e93f36c
Eileen Neumann <eileen@maschinendeck.org>
2020-08-04 18:20:19 +0200

    Added deposit page to make deposits

---
+++ pages/deposit/page.php
@@ -0,0 +1,27 @@
+<?php
+
+    if (isset($_GET["value"]) && $currentUser) {
+        $currentUser->balance += $_GET["value"];
+        $store->writeToDisk();
+        echo "<meta http-equiv=\"refresh\" content=\"0;url=?page=index\">";
+    } else {
+        $depositOptions = array(
+            array("value" =>   100, "img" =>   "100.jpg"),
+            array("value" =>   200, "img" =>   "200.jpg"),
+            array("value" =>   500, "img" =>   "500.jpg"),
+            array("value" =>  1000, "img" =>  "1000.jpg"),
+            array("value" =>  2000, "img" =>  "2000.jpg"),
+            array("value" =>  5000, "img" =>  "5000.jpg"),
+            array("value" => 10000, "img" => "10000.jpg"),
+            array("value" => 20000, "img" => "20000.jpg")
+        );
+    
+        foreach ($depositOptions as $option) {
+            $bindingsCharge = array("href" => $currentUser ? "?page=deposit&value=" . $option["value"]  : "?page=user_signin",
+                                    "value" => number_format($option["value"] / 100.0, 2),
+                                    "image" => $option["img"]);
+            bindAndRenderTemplate(__DIR__ . "/template_deposit_option.html", $bindingsCharge);
+        }
+    }
+
+?>
