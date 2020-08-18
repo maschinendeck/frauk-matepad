a3b4eceafe928ec894a830194bde5367b2f99ada
Michael Ochmann <michael.ochmann@propeller.de>
2020-08-19 01:38:28 +0200

    made css fancier

--- pages/deposit/page.php
+++ pages/deposit/page.php
@@ -14,21 +14,23 @@
             array("value" =>  2000, "img" =>  "2000.jpg"),
             array("value" =>  5000, "img" =>  "5000.jpg"),
         );
-    
+
+        echo "<div class='index-content'>";
         foreach ($depositOptions as $option) {
-            // Generate link 
+            // Generate link
             $linkTo = $currentUser ? "?page=deposit&value=" . $option["value"] : "?page=user_signin";
             if (isset($option["link"])) {
                 $linkTo = $option["link"];
             }
 
             $bindingsCharge = array("href" => $linkTo,
-                                    "value" => is_numeric($option["value"]) ? 
+                                    "value" => is_numeric($option["value"]) ?
                                                 number_format($option["value"] / 100.0, 2) . "€":
                                                 $option["value"],
                                     "image" => $option["img"]);
             bindAndRenderTemplate("deposit_option.html", $bindingsCharge);
         }
+        echo "</div>";
     }
 
 ?>
